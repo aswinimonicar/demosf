ec84c5dbc8b573926a5c05c316acf3907be4d693
aswinimonicar <141112276+aswinimonicar@users.noreply.github.com>
2025-04-30 14:18:03 +0530

    Create deploy.php

---
+++ deploy.php
@@ -0,0 +1,40 @@
+<?php
+namespace Deployer;
+
+require 'recipe/symfony.php';
+
+set('application', 'DemoFG');
+
+// Config
+
+set('repository', 'git@github.com:aswinimonicar/demosf.git');
+
+add('shared_files', []);
+add('shared_dirs', ['var/log', 'var/cache']);
+//add('shared_dirs', []);
+add('writable_dirs', ['var']);
+//add('writable_dirs', []);
+
+// Hosts
+host('production')
+    ->setHostname(10.10.100.57')
+    ->setRemoteUser('fairgateadmin')
+    ->setDeployPath('/home/fairgateadmin/demosf');
+//host('10.10.100.57')
+//    ->set('remote_user', 'deployer')
+//    ->set('deploy_path', '~/demosf');
+
+// Tasks
+
+desc('Custom deploy sequence');
+task('deploy', [
+    'deploy:prepare',
+    'deploy:vendors',
+    'deploy:cache:clear',
+    'deploy:publish',
+]);
+
+
+// Hooks
+
+after('deploy:failed', 'deploy:unlock');
